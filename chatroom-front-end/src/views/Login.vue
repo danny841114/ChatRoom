b2a5a99307f39027436a38af13ea1ec559e4027f
Danny Lin <danny841114@gmail.com>
2026-05-08 23:23:10 +0800

    add quick login

--- chatroom-front-end/src/views/Login.vue
+++ chatroom-front-end/src/views/Login.vue
@@ -1,83 +1,130 @@
 <template>
-    <div
-      class="container mt-5 justify-content-center align-items-center"
-      style="width: 300px"
-    >
-      <h3 class="text-center">登入</h3>
-      <div>
-        <form @submit.prevent="handleLogin">
-          <div class="input-group mb-3">
-            <span class="input-group-text">帳號</span>
-            <input
-              type="text"
-              class="form-control"
-              aria-label="account"
-              v-model="account"
-              required
-            />
-          </div>
-  
-          <div class="input-group mb-3">
-            <span class="input-group-text">密碼</span>
-            <input
-              type="password"
-              class="form-control"
-              aria-label="account"
-              v-model="password"
-              required
-            />
-          </div>
-  
-          <div class="text-center">
-            <button type="submit" class="btn btn-primary">登入</button>
-          </div>
-        </form>
-      </div>
+  <div class="container mt-5 justify-content-center align-items-center overall">
+    <h3 class="text-center">登入</h3>
+    <div>
+      <form @submit.prevent="handleLogin">
+        <div class="input-group mb-3">
+          <label class="input-group-text" for="account">帳號</label>
+          <input
+            type="text"
+            class="form-control"
+            id="account"
+            v-model="account"
+            required
+          />
+        </div>
+
+        <div class="input-group mb-3">
+          <label class="input-group-text" for="password">密碼</label>
+          <input
+            type="password"
+            class="form-control"
+            id="password"
+            v-model="password"
+            required
+          />
+        </div>
+
+        <div class="text-center">
+          <button type="submit" class="btn btn-primary">登入</button>
+        </div>
+      </form>
     </div>
-  </template>
-  
-  <script setup>
-  import { ref } from "vue";
-  import { useRouter } from "vue-router";
-  import { useAuthStore } from "@/stores/auth";
-  import axios from "axios";
-  import Swal from "sweetalert2";
 
-  const apiBase = import.meta.env.VITE_API_BASE_URL;
-  const account = ref("");
-  const password = ref("");
-  const errorMsg = ref("");
-  const router = useRouter();
-  const authStore = useAuthStore();
-  
-  const handleLogin = async () => {
-    try {
-      const response = await axios.post(`${apiBase}/api/auth/login`, {
-        account: account.value,
-        password: password.value,
-      });
-      console.log("response.data",response.data)
+    <hr />
 
-      authStore.setLogin(response.data);
-  
-      await Swal.fire({
-        title: "登入成功",
-        icon: "success",
-        text: "確定後跳轉首頁",
-        confirmButtonText: "確定",
-      });
-  
-      router.replace("/");
-    } catch (error) {
-      errorMsg.value = error.response.data.message;
-      await Swal.fire({
-        title: "登入失敗",
-        icon: "error",
-        text: errorMsg.value,
-        confirmButtonText: "確定",
-      });
-    }
-  };
-  </script>
-  
-  <style></style>
+    <div class="text-center quick-login">
+      <h3>快速登入</h3>
+      <button
+        class="btn btn-success quick-login-btn"
+        @click="quickLogin('user01', 'aaa123')"
+      >
+        user01
+      </button>
+      <button
+        class="btn btn-success quick-login-btn"
+        @click="quickLogin('user02', 'bbb456')"
+      >
+        user02
+      </button>
+      <button
+        class="btn btn-success quick-login-btn"
+        @click="quickLogin('user03', 'ccc789')"
+      >
+        user03
+      </button>
+      <button
+        class="btn btn-success quick-login-btn"
+        @click="quickLogin('user04', 'ddd101')"
+      >
+        user04
+      </button>
+      <button class="btn btn-success" @click="quickLogin('user05', 'eee202')">
+        user05
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+import { useAuthStore } from "@/stores/auth";
+import axios from "axios";
+import Swal from "sweetalert2";
+
+const apiBase = import.meta.env.VITE_API_BASE_URL;
+const account = ref("");
+const password = ref("");
+const errorMsg = ref(null);
+const router = useRouter();
+const authStore = useAuthStore();
+
+const handleLogin = async () => {
+  try {
+    const response = await axios.post(`${apiBase}/api/auth/login`, {
+      account: account.value,
+      password: password.value,
+    });
+
+    authStore.setLogin(response.data);
+
+    await Swal.fire({
+      title: "登入成功",
+      icon: "success",
+      text: "確定後跳轉首頁",
+      confirmButtonText: "確定",
+    });
+
+    router.replace("/");
+  } catch (error) {
+    errorMsg.value = error.response.data.message;
+    await Swal.fire({
+      title: "登入失敗",
+      icon: "error",
+      text: errorMsg.value,
+      confirmButtonText: "確定",
+    });
+  }
+};
+
+const quickLogin = async (a, b) => {
+  account.value = a;
+  password.value = b;
+  handleLogin();
+};
+</script>
+
+<style scoped>
+.overall {
+  width: 60%;
+}
+
+.quick-login {
+  margin-top: 20px;
+}
+
+.quick-login-btn {
+  margin-right: 10px;
+}
+</style>
